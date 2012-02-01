de96e685e09f59d88a13d943aae0f7805b1b8d85
Maxime Haineault <max@motion-m.ca>
2012-02-01 10:42:25 -0500

    Created detail view

--- newsly/urls.py
+++ newsly/urls.py
@@ -3,5 +3,5 @@ from newsly.views import *
 
 urlpatterns=patterns('',
     url(r'^page/(?P<page>[0-9]+)/$', NewsView.as_view(), name="newsly-index"),
-    url(r'^(?P<slug>[\-\d\w]+)/$', NewsDetail.as_view(), name='newsly-detail'),
+    url(r'^(?P<slug>[\-\d\w]+)/$', NewsDetail.as_view(), name="newsly-detail"),
 )
