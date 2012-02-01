9027e4b1a7f5a735da4a062e95be547d82b0924c
Maxime Haineault <max@motion-m.ca>
2012-02-01 15:58:00 -0500

    Added configuration for tinymce (with grappelli)

---
+++ newsly/static/newsly/js/tinymce_setup.js
@@ -0,0 +1,47 @@
+tinyMCE.init({
+    // General options
+    mode : "textareas",
+    theme : "advanced",
+    plugins : "autolink,lists,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,wordcount,advlist,autosave",
+
+    // Theme options
+    theme_advanced_buttons1 : "save,newdocument,|,cut,copy,paste,pasteword,|,undo,redo,|,search,|,outdent,indent,|,bullist,numlist,|,insertdate,inserttime,preview,|,hr,charmap,fullscreen,removeformat,cleanup,",
+    theme_advanced_buttons2 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,formatselect,fontsizeselect,|,forecolor,backcolor,sub,sup|,link,unlink,",
+    theme_advanced_buttons3 : "",
+    theme_advanced_toolbar_location : "top",
+    theme_advanced_toolbar_align : "left",
+    theme_advanced_statusbar_location : "bottom",
+    theme_advanced_resizing : true,
+
+    // Example content CSS (should be your site CSS)
+    //content_css : "/static/website/css/site.css",
+
+    // Drop lists for link/image/media/template dialogs
+    /*
+    template_external_list_url : "lists/template_list.js",
+    external_link_list_url : "lists/link_list.js",
+    external_image_list_url : "lists/image_list.js",
+    media_external_list_url : "lists/media_list.js",
+    */
+
+    // Style formats
+    /*
+    style_formats : [
+        {title : 'Bold text', inline : 'b'},
+        {title : 'Red text', inline : 'span', styles : {color : '#ff0000'}},
+        {title : 'Red header', block : 'h1', styles : {color : '#ff0000'}},
+        {title : 'Example 1', inline : 'span', classes : 'example1'},
+        {title : 'Example 2', inline : 'span', classes : 'example2'},
+        {title : 'Table styles'},
+        {title : 'Table row 1', selector : 'tr', classes : 'tablerow1'}
+    ],
+    */
+
+    // Replace values for the template plugin
+    /*
+    template_replace_values : {
+        username : "Some User",
+        staffid : "991234"
+    }
+    */
+});
