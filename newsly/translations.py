36ebb984be2ff03d48cea20788d5c6882bc5fd19
Dominic Montreuil <dominic.montreuil@gmail.com>
2012-02-13 11:30:32 -0500

    translations...

---
+++ newsly/translations.py
@@ -0,0 +1,21 @@
+from newsly.models import News, NewsPhoto, NewsVideo, NewsDocument
+
+class NewsTranslationOptions(TranslationOptions):
+    fields = ('title', 'teaser', 'body',)
+translator.register(News, NewsTranslationOptions)
+
+
+class NewsPhotoTranslationOptions(TranslationOptions):
+    fields = ('title', )
+translator.register(NewsPhoto, NewsPhotoTranslationOptions)
+
+
+class NewsVideoTranslationOptions(TranslationOptions):
+    fields = ('title', )
+translator.register(NewsVideo, NewsVideoTranslationOptions)
+
+
+class NewsDocumentTranslationOptions(TranslationOptions):
+    fields = ('title', )
+translator.register(NewsDocument, NewsDocumentTranslationOptions)
+
