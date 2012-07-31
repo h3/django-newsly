264e25e28ee5f3dc146e27fba90bc45d82d79952
Maxime Haineault <max@motion-m.ca>
2012-07-31 10:06:25 -0400

    Refactored get_latest_news templatetag

--- newsly/templatetags/newsly_tags.py
+++ newsly/templatetags/newsly_tags.py
@@ -9,41 +9,23 @@ from newsly.models import News
 register = template.Library()
 
 
-class LatestNewsNode(template.Node):
-    def __init__(self, varname, limit=1):
-        self.varname = varname
-        self.limit = limit
+@register.assignment_tag
+def get_latest_news(*args, **kwargs):
+    """
+    Returns a sorted list of news
+
+    {% get_latest_news author=1 category=2 limit=10 as latest_news %}
+    """
+    limit    = kwargs.get('limit', None)
+    author   = kwargs.get('author', None)
+    category = kwargs.get('category', None)
+    qs       = News.published.all()
+
+    if author:   qs = qs.filter(author_id=author)
+    if category: qs = qs.filter(category_id=category)
     
-    def render(self, context):
-        try:
-            rs = News.published.all()[0:self.limit]
-            if self.limit == 1:
-                context[self.varname] = rs[0]
-            else:
-                context[self.varname] = rs[0:self.limit]
-        except:
-            pass
-        return ''
+    return limit and qs[0:limit] or qs
 
-@register.tag(name="get_latest_news")
-def get_latest_news(parser, token):
-    bits = token.contents.split()
-    if len(bits) != 3 and len(bits) != 5:
-        raise template.TemplateSyntaxError, "get_latest_news tag takes either two or four arguments"
-    if bits[1] != 'as':
-        raise template.TemplateSyntaxError, "first argument to the get_latest_news tag must be 'as'"
-    if len(bits) == 5:
-        if bits[3] != 'limit':
-            raise template.TemplateSyntaxError, "third argument to the get_latest_news tag must be 'limit'"
-        limit = bits[4]
-    else:
-        limit = 1
-    try:
-        limit = int(limit)
-    except ValueError:
-        raise template.TemplateSyntaxError, "Forth argument to the get_latest_news tag must an integer"
-
-    return LatestNewsNode(bits[2], limit)
 
 @register.simple_tag()
 def embed_player(v, w):
@@ -57,6 +39,7 @@ def embed_player(v, w):
         return html % {'w': w, 'src': v, 'h': int(int(w) * 0.75) }
     except:
         return ''
+
 
 @register.filter()
 def get_file_extension(f):
